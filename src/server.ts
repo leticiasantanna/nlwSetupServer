62ba6c60202b0da3cddb42e9e5cea70973c64f54
Leticia Santanna <leticiafsn_@hotmail.com>
2023-01-21 18:28:54 -0300

    first configs prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "Eawwwwwww";
+app.register(cors);
+// app.register(cors, {
+//origin: ['localhost:3000']});
+//utilizando origin posso determinar quais app front-end podem acessar este back
+
+app.get("/hello", async () => {
+  const habito = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "Fazer",
+      },
+    },
+  });
+  return habito;
 });
 
 app.listen({
